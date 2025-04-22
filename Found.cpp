779f24327edeb18348687a088869fa6be68f1bee
Ali <alimovmuhammadali01222008@gmail.com>
2025-04-22 14:37:09 +0500

    initial commit

--- Found.cpp
+++ Found.cpp
@@ -3,20 +3,10 @@
 #include <string>
 #include <iomanip>
 #include <vector>
-#include <limits>
+
 
 #include "myfunshare.h"
 #include "Item.cpp"
 
 using namespace std;
 
-int main()
-{
-    int option;
-
-    option=datatype_checker<int>("Enter your choice: ");
-
-    cout<<option<<endl;
-
-    return 0;
-}
--- Lost.cpp
+++ Lost.cpp
@@ -16,20 +16,4 @@ using namespace std;
 //     Lost_database(int ID,string name,string description,int year,int month,int day,string location,int contuct_number):
 //     Item(ID,name,description,year,month,day,location,contuct_number){}
 
-//     ofstream L_database;
-    
 
-//     void data_input(){
-//         L_database.open("lost_database.csv");
-//         L_database<<ID<<",";
-//     }
-    
-// };
-
-int main()
-{   
-    string name="hamster";
-    bool AAA=Lost_check_found_database(name);
-    cout<<"\n"<<AAA<<endl;
-    return 0;
-}
